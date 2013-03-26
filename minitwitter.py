176a805c5fe7c48cf60d9e1938a054000a86dec7
Alex Morega <alex@grep.ro>
2013-03-26 22:36:52 +0200

    save messages to db

--- minitwitter.py
+++ minitwitter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from datetime import datetime
 import flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -23,10 +24,14 @@ def home():
 @app.route('/new', methods=['GET', 'POST'])
 def new():
     if flask.request.method == 'POST':
-        print "new message:", flask.request.form['message']
+        text = flask.request.form['message']
+        message = Message(text=text, time=datetime.utcnow())
+        db.session.add(message)
+        db.session.commit()
         return flask.redirect(flask.url_for('home'))
     return flask.render_template('new.html')
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
